ba891aaad2d861d942d39f0cf894aa24116f4491
dwolgast-lab <dwolgast@gmail.com>
2026-03-05 15:42:20 -0500

    hotfix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 /* =========================================================================
  * MASL 3 4th Official Log App
  * Author: Dave Wolgast
- * Version: 0.41
+ * Version: 0.42
  * ========================================================================= */
 
 import { useState, useEffect } from 'react';
@@ -20,7 +20,7 @@ import PenaltyModal from './components/modals/PenaltyModal';
 import TimeKeypadModal from './components/modals/TimeKeypadModal';
 import PlayerSelectModal from './components/modals/PlayerSelectModal';
 
-const APP_VERSION = "0.41";
+const APP_VERSION = "0.42";
 
 let audioCtx = null;
 const initAudio = () => {
